71256186032c3e08adad058f8e7101b0b5c65dc1
Jhone Mendonça <g.m.enohj@gmail.com>
2016-04-19 15:03:20 -0400

    Questão 9

--- 9.sql
+++ 9.sql
@@ -1,8 +1,12 @@
 --9) Os nomes dos candidatos que não obtiveram nenhum voto na eleição de 2006.
 
-SELECT DISTINCT(C.NOME)
-FROM CANDIDATO C, VOTACAO V
-WHERE C.NUMERO = V.CANDIDATO
-AND V.ANO <> 2006
-
+SELECT C.NOME
+FROM CANDIDATO C
+WHERE C.NUMERO NOT IN 
+(
+    SELECT C.NUMERO
+    FROM CANDIDATO C, VOTACAO V
+    WHERE C.NUMERO = V.CANDIDATO
+    AND V.ANO = 2006
+)
 --babá, edu braga, acrenino, sabino, joao pedro, lula, serafim, eron, aninha, barbosao, vanessa, 
